87c28a31d85c6695a8841243af639bce925c9e70
ksarapultsev <ksarapultsev2014@gmail.com>
2019-10-23 18:16:06 +0600

    12 - SportsStore - Security & Deployment

---
+++ 12 - SportsStore - Security & Deployment/SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace SportsStore.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void Index_Contains_All_Products()
+        {
+            // Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }).AsQueryable<Product>());
+            // Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+            // Действие
+            Product[] result
+                = GetViewModel<IEnumerable<Product>>(target.Index())?.ToArray();
+            // Утверждение
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+
+        [Fact]
+        public void Can_Edit_Product()
+        {
+            // Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }).AsQueryable<Product>());
+            // Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+
+            // Действие
+            Product p1 = GetViewModel<Product>(target.Edit(1));
+            Product p2 = GetViewModel<Product>(target.Edit(2));
+            Product p3 = GetViewModel<Product>(target.Edit(3));
+
+            // Утверждение
+            Assert.Equal(1, p1.ProductID);
+            Assert.Equal(2, p2.ProductID);
+            Assert.Equal(3, p3.ProductID);
+        }
+
+        [Fact]
+        public void Can_Edit_Nonexistent_Product()
+        {
+            // Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }).AsQueryable<Product>());
+            // Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+
+            // Действие
+            Product result = GetViewModel<Product>(target.Edit(4));
+           
+            // Утверждение
+            Assert.Null(result);           
+        }
+
+        [Fact]
+        public void Can_Save_Valid_Changes()
+        {
+            // Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            // Организация - создание имитированных временных данных
+            Mock<ITempDataDictionary> tempData = new Mock<ITempDataDictionary>();
+
+            // Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object)
+            {
+                TempData = tempData.Object
+            };
+            // Организация - создание товара
+            Product product = new Product { Name = "Test" };
+
+            // Действие - попытка сохранить товар
+            IActionResult result = target.Edit(product);
+            // Утверждение - проверка того, что к хранилищу было произведено обращение
+            mock.Verify(m => m.SaveProduct(product));
+            // Утверждение - проверка, что типом результата является перенаправление
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", (result as RedirectToActionResult).ActionName);
+
+        }
+
+        [Fact]
+        public void Cannot_Save_Invalid_Changes()
+        {
+            // Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            // Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+          
+            // Организация - создание товара
+            Product product = new Product { Name = "Test" };
+            // Организация - добавление ошибки в состояние модели
+            target.ModelState.AddModelError("error", "error");
+
+            // Действие - попытка сохранить товар
+            IActionResult result = target.Edit(product);
+            // Утверждение - проверка того, что к хранилищу было произведено обращение
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
+            // Утверждение - проверка, что типом результата является метода
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Cannot_Delete_Valid_Products()
+        {
+            // Организация - создание объекта Product
+            Product prod = new Product { ProductID = 2, Name = "Test" };
+            // Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+           {
+                new Product {ProductID = 1, Name = "P1"},
+                prod,
+                new Product {ProductID = 3, Name = "P3"},
+           }).AsQueryable<Product>());
+            // Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+
+            // Действие - удаление товара
+            target.Delete(prod.ProductID);
+
+            // Утверждение - проверка того, что был вызван метод удаления
+            // в хранилище с корректным объектом Product
+            mock.Verify(m => m.DeleteProduct(prod.ProductID));
+        }
+
+    }
+}
---
+++ 12 - SportsStore - Security & Deployment/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using SportsStore.Components;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            // Организация
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1 , Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            //Действие - получение набора категорий
+            string[] results = ((IEnumerable<string>)(target.Invoke()
+                as ViewViewComponentResult).ViewData.Model).ToArray();
+
+            // Утверждение
+            Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, results));
+        }
+
+        [Fact]
+        public void Indicates_Select_Categories()
+        {
+            // Организация
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1 , Name = "P1", Category = "Apples"},             
+                new Product {ProductID = 4,  Name = "P2", Category = "Oranges"},
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext { ViewContext = new Microsoft.AspNetCore.Mvc.Rendering.ViewContext { RouteData = new Microsoft.AspNetCore.Routing.RouteData() } };
+            target.RouteData.Values["category"] = categoryToSelect;
+            
+            //Действие - получение набора категорий
+            string result = (string)(target.Invoke()
+                as ViewViewComponentResult).ViewData["SelectedCategory"];
+
+            // Утверждение
+            Assert.Equal(categoryToSelect, result);
+        }
+    }
+}
