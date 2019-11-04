10626df7aa529069eeb48cd445aaa9b9350b9569
ksarapultsev <ksarapultsev2014@gmail.com>
2019-11-05 02:24:30 +0600

    15 - URL Routing/Urls

---
+++ 15 - URL Routing/UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+   [Route("app/[controller]/actions/[action]/{id:weekday?}")]
+    public class CustomerController : Controller
+    {
+        //[Route("myroute")]
+       // [Route("[controller]/MyAction")]
+        public ViewResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(CustomerController),
+                Action = nameof(Index)
+            });
+
+        public ViewResult List(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(List),
+            };
+            r.Data["Id"] = id ?? "<no value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
+    }
+}
---
+++ 15 - URL Routing/UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(Index)
+            });
+        public ViewResult CustomVariable(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(CustomVariable),
+            };
+            r.Data["Id"] = id ?? "<no value>";
+            return View("Result", r);
+        }
+        
+    }
+}
